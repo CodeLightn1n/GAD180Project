a495549b8aa030a74054c82a43385368f4a5df3e
CodeLightn1n <68534343+CodeLightn1n@users.noreply.github.com>
2020-10-13 11:22:50 +0800

    Movement and Jump

--- GAD180Project/Assets/Scripts/Movement2.cs
+++ GAD180Project/Assets/Scripts/Movement2.cs
@@ -7,27 +7,46 @@ public class Movement2 : MonoBehaviour
     Rigidbody2D rg2d;
     Vector2 movementForce;
     [SerializeField] float maxSpeed;
+    [SerializeField] float jumpHeight;
+    bool grounded;
+    
     private void Start()
     {
         rg2d = GetComponent<Rigidbody2D>();
         movementForce = new Vector2(1f, 0f);
         maxSpeed = 25f;
+        jumpHeight = 10f;
+        grounded = true;
     }
-    private void FixedUpdate()
+    private void movement()
     {
         //Move Right
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             rg2d.AddForce(movementForce, ForceMode2D.Impulse);
+            this.gameObject.transform.localScale = new Vector2(1.5f, 1.5f);
         }
         //Move Left
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             rg2d.AddForce(-movementForce, ForceMode2D.Impulse);
+            this.gameObject.transform.localScale = new Vector2(-1.5f, 1.5f);
         }
-        if(rg2d.velocity.magnitude > maxSpeed)
+        //Max Speed default is 25
+        if (rg2d.velocity.magnitude > maxSpeed)
         {
             rg2d.velocity += rg2d.velocity.normalized * maxSpeed;
         }
+        if(grounded)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rg2d.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        movement();
     }
 }
