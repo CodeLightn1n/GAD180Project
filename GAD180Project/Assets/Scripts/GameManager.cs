1ff8efdea92250f62a71d2798a951451a9700e74
CodeLightn1n <68534343+CodeLightn1n@users.noreply.github.com>
2020-11-16 19:52:42 +0800

    Player Prefab Update and Game Manager
    added in a game manager, script to be attached to an empty object and the player now has a player script so it can use the game manager's functions

---
+++ GAD180Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject player;
+    public GameObject currentRespawn;
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    public void PlayerDied()
+    {
+        Instantiate(player.gameObject, currentRespawn.transform.position, Quaternion.identity);
+    }
+}
