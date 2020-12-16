77ada2e2ccf41619fdc1fe1d2fa158272259071f
Tomas123GitHub <61271366+Tomas123GitHub@users.noreply.github.com>
2020-12-16 16:50:23 +0800

    Map edits and main menu buttons

--- GAD180Project/Assets/Scripts/MainMenu.cs
+++ GAD180Project/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ButtonMoveScene(string level)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(level);
     }
 }
